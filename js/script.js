b55d77231e5e5e572e7ee3c40b6fe60ce526f0c6
Khusiima Luke Masaba <masabaluke@gmail.com>
2021-11-17 22:13:14 +0300

    add mobile menu

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const openMobileMenu = document.querySelectorAll('[data-mobile-menu-target]');
+const mainTag = document.querySelector('.main');
+const closeMobileMenu = document.querySelectorAll('[data-close-button]');
+
+function openMenu(mobileMenu, mainTag) {
+  if (mobileMenu == null) return;
+  mobileMenu.classList.add('active');
+  mainTag.classList.add('blur');
+}
+
+function closeMenu(mobileMenu) {
+  if (mobileMenu == null) return;
+  mobileMenu.classList.remove('active');
+  mainTag.classList.remove('blur');
+}
+openMobileMenu.forEach((button) => {
+  button.addEventListener('click', () => {
+    const mobileMenu = document.querySelector(button.dataset.mobileMenuTarget);
+    openMenu(mobileMenu, mainTag);
+  });
+});
+
+closeMobileMenu.forEach((button) => {
+  button.addEventListener('click', () => {
+    const mobileMenu = button.closest('.mobile-menu');
+    closeMenu(mobileMenu);
+  });
+});
