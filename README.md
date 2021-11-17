527469011d28f45634e2ee8390011ad5cc53bdf1
Khusiima Luke Masaba <masabaluke@gmail.com>
2021-11-18 00:53:55 +0300

    add readme.md

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# gaming_tournament
-This is a website describing an international gaming tournament
+![](https://img.shields.io/badge/Microverse-blueviolet)
+# Gaming tournament
+
+> Warzone Gaming Toirnament
+
+![screenshot](./pics/desktop.png)
+![screenshot](./pics/mobile.png)
+
+
+
+## Built With
+
+- HTML
+- CSS
+- Javascript
+
+## Getting Started
+
+
+To get a local copy up and running follow these simple example steps.
+
+### Download 
+https://github.com/Lucas-Masaba/gaming_tournament.git
+ 
+Follow this link to my GitHub repository. Copy the link from the code button
+ 
+After you have copied the link, enter the command below in your terminal
+ 
+`git clone https://github.com/Lucas-Masaba/gaming_tournament.git`
+
+## Remember to paste the link after the git clone command 
+
+## Authors
+
+👤 **Khusiima Luke Masaba**
+
+- GitHub: [@githubhandle](https://github.com/Lucas-Masaba)
+- Twitter: [@twitterhandle](https://twitter.com/MasabaLuke)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/khusiima-luke-masaba-59060a121)
+
+## Live Demo
+
+[Here](https://lucas-masaba.github.io/gaming_tournament/) is a link to the online view of my website
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- js/script2.js
+++ js/script2.js
@@ -1,53 +1,64 @@
-const listItems = document.querySelectorAll('.item');
 const speakers = [
   {
-
+    id: '0',
     image: './pics/zlaner.jpg',
     name: 'Zach Laner',
     job: 'Youtuber and back to back tournament champion',
     description: 'Zach Laner a.k.a zlaner is a skilled youtube streamer and has won multiple tournaments',
   },
   {
-
+    id: '1',
     image: './pics/doc.jpg',
     name: 'Dr Disrepect',
     job: 'Youtube streamer',
     description: 'The Doc is a back to back 2x 90s champion.',
   },
   {
+    id: '2',
     image: './pics/tim.jpg',
     name: 'Tim',
     job: 'Streamer',
     description: "Tim also know as 'tim the tatman' is an American streamer",
   },
   {
+    id: '3',
     image: './pics/jason.jpg',
     name: 'Jason Blundell',
     job: 'Co-Studio Head and Game Director at Treyarch',
     description: 'Blundell started working on the Call of Duty franchise as a Producer for Call of Duty 3. He became Executive Producer on Call of Duty: Black Ops and was one the Campaign Design Directors for Call of Duty: Black Ops II.',
   },
   {
+    id: '4',
     image: './pics/matthew.jpg',
     name: 'Matthew Lewis',
     job: 'Vice President of Product & GM, Call of Duty: Mobile · Activision',
     description: 'Lewis has been working for activision for a while now and has been appointed to lead the call of duty mobile team',
   },
   {
+    id: '5',
     image: './pics/amos.jpg.crdownload',
     name: 'Amos Hodge',
     job: 'Director of call of duty warzone',
     description: 'One of the best battle royales ever made, Amos directs warzone and is the game to be played at this tournament',
   },
+
 ];
-listItems.forEach((speaker) => {
-  const speakerId = speaker.getAttribute('id');
-  const guestSection = document.querySelectorAll('.guests_section');
-  const speakerImage = guestSection.querySelector('.guest_img_div');
-  const speakerName = guestSection.querySelector('.name');
-  const speakerJob = guestSection.querySelector('.work');
-  const speakerDescription = guestSection.querySelector('.work_description');
-  speakerImage.src = speakers[speakerId].image;
-  speakerName.innerHTML = speakers[speakerId].name;
-  speakerJob.innerHTML = speakers[speakerId].job;
-  speakerDescription.innerHTML = speakers[speakerId].description;
+const createSpeaker = () => {
+  const addSpeaker = document.querySelector('#guests');
+  speakers.forEach((speaker) => {
+    addSpeaker.insertAdjacentHTML('beforeend',
+      `
+      <li id="0" class="item">
+          <div class="guest_img_div"><img class="guest_img" src="${speaker.image}" alt=""></div>
+          <div class="guest_paragraphs">
+            <p class="name">${speaker.name}</p>
+            <p class="work">${speaker.job}</p>
+            <p class="work_description">${speaker.description}</p>
+          </div>
+        </li>
+    `);
+  });
+};
+document.addEventListener('DOMContentLoaded', () => {
+  createSpeaker();
 });
